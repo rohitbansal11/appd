cb0ac8a6ca02a08022d2229b10ba89edd4dd0ee3
Shubham Kashyap <hex.media.1800@gmail.com>
2022-02-18 14:38:38 +0530

    word of day added & legal term wishlist added

--- controllers/WordOfDay/WordOfDayAdmin.js
+++ controllers/WordOfDay/WordOfDayAdmin.js
@@ -1,22 +1,19 @@
-import asyncHandler from "express-async-handler";
-import WordOfDayModal from "../../models/WordOfDay.js";
+import asyncHandler from 'express-async-handler'
+import WordOfDayModal from '../../models/WordOfDay.js'
 
 // @desc        Get Word Of Day
 // @route       GET   /api/v1/word-of-day
 // @access      Private
 export const setNewWordOfDay = asyncHandler(async (req, res) => {
-  const wordId = req.params.wordId;
-  // set a new random word as word of the day
-  console.log(wordId);
+  const wordId = req.params.wordId
   // get current word of the day
-  let wordOfDay = await WordOfDayModal.find();
-  console.log(wordOfDay);
-  wordOfDay = wordOfDay[0];
+  let wordOfDay = await WordOfDayModal.find()
+  wordOfDay = wordOfDay[0]
 
   // // change the id if word of the day
   const newWord = await WordOfDayModal.findByIdAndUpdate(wordOfDay._id, {
     dictionary_id: wordId,
-  });
+  })
 
-  res.status(201).json(newWord);
-});
+  res.status(201).json(newWord)
+})
