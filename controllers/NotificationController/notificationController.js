cb68f2197ed0b77cc70e9161548378ca2b2e6b55
rohitbansal11 <rbansal000888@gmail.com>
2022-03-05 18:58:18 +0530

    notification fixed

--- controllers/NotificationController/notificationController.js
+++ controllers/NotificationController/notificationController.js
@@ -11,14 +11,18 @@ export const getNotification = asyncHandler(async (req, res, next) => {
     let id = notificationcheck[0]._id
     var today_date = new Date();
     var compare_date = new Date(notificationcheck[0].verifyDate);
-    var today_date_check = today_date.getDay() + ":" + today_date.getMonth() + ":" + today_date.getFullYear();
-    var compare_date_check = compare_date.getDay() + ":" + compare_date.getMonth() + ":" + compare_date.getFullYear();
-    let compare = today_date_check == compare_date_check
+    var today_date_check = today_date.getDay() + ":" + today_date.getMonth() + ":" + today_date.getFullYear()+ 'T' + today_date.getHours() + ':' + today_date.getMinutes();
+    var compare_date_check = compare_date.getDay() + ":" + compare_date.getMonth() + ":" + compare_date.getFullYear()+ 'T' + compare_date.getHours() + ':' + compare_date.getMinutes();
+    let compare = today_date_check < compare_date_check
+
     if (compare) {
+       
         SentAllNotification(200, res)
     } else {
-        await NotificationCheck.findByIdAndUpdate(id, { verifyDate: today_date })
-        CheckExpireDate(200, res)
+        var today_date_for_expire = new Date();
+        today_date_for_expire.setHours( today_date_for_expire.getHours() + 1 );
+        await NotificationCheck.findByIdAndUpdate(id, { verifyDate: today_date_for_expire })
+       CheckExpireDate(200, res)
     }
 })
 
@@ -34,16 +38,14 @@ const SentAllNotification = async (statusCode, res) => {
 const CheckExpireDate = async (statusCode, res) => {
     let AllNotification = await Notification.find({})
     var today_date = new Date();
-    var today_date_check = today_date.getDay() + ":" + today_date.getMonth() + ":" + today_date.getFullYear();
+    var today_date_check = today_date.getDay() + ":" + today_date.getMonth() + ":" + today_date.getFullYear() + 'T' + today_date.getHours() + ':' + today_date.getMinutes();
     let arr = []
     await AllNotification.map(async (w) => {
         var compare_date = new Date(w.expiredate);
-        var expire_date_check = compare_date.getDay() + ":" + compare_date.getMonth() + ":" + compare_date.getFullYear();
-        let compare = today_date_check == expire_date_check
-        console.log(compare)
+        var expire_date_check = compare_date.getDay() + ":" + compare_date.getMonth() + ":" + compare_date.getFullYear() + 'T' + compare_date.getHours() + ':' + compare_date.getMinutes();
+        let compare = today_date_check > expire_date_check
         if (compare) {
-            let dddd = await Notification.findByIdAndDelete(w._id)
-            console.log(dddd)
+             await Notification.findByIdAndDelete(w._id)
         } else {
             arr.push(w)
         }
